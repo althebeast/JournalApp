3ed9f3e0a01ea75ff6c1aa4a0c46b3cddf549d11
althebeast <alperensarisan@icloud.com>
2023-12-09 15:16:10 -0400

    Initial Commit

---
+++ JournalApp/JournalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalAppApp.swift
+//  JournalApp
+//
+//  Created by Alperen Sarışan on 9.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct JournalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
